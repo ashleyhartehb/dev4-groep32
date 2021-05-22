dc5319b98cfeaa2cb509512862e1a00fc8b70cf9
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2021-05-22 15:16:36 +0200

    Eerste code classe picture

--- src/main/java/factorymethod/cache/Picture.java
+++ src/main/java/factorymethod/cache/Picture.java
@@ -5,5 +5,26 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Picture {
+    private final double timestamp;
+    private final String topic;
+    private final String username;
+    private final String description;
+    private final String resolution;
+
+    private static final Map<String, Picture> pictures = new HashMap<>();
+
+    private Picture(double timestamp, String topic, String username, String description, String resolution) {
+        this.timestamp = timestamp;
+        this.topic = topic;
+        this.username = username;
+        this.description = description;
+        this.resolution = resolution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof  Picture)) return false;
+    }
 
 }
