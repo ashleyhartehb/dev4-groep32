487e92b4fa6cda16707cf7ba74753243e12c51e9
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2021-05-23 21:14:45 +0200

    Aanmaken van parition function en commentaar

--- Algoritme.js
+++ Algoritme.js
@@ -7,3 +7,22 @@ function swap(colorValues, leftIndex, rightIndex){
     colorValues[leftIndex] = colorValues[rightIndex];
     colorValues[rightIndex] = temp;
 }
+function partition(colorValues, left, right){
+    //Variabelen aanmaken voor middelste en uiterste elementen
+    var pivot = colorValues[Math.floor((right+left)/2)], i = left, j = right;
+    while(i<=j){
+        while(colorValues[i] < pivot){
+            i++;
+        }
+        while(colorValues[j] > pivot){
+            j--;
+        }
+        if(i<=j){
+            //Omwisselen van de 2 elementen
+            swap(colorValues, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
